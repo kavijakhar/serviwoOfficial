790f05e8fa777f4cf06811451b2c1ce521201e90
Kavita Jakhar <Kavya21@gmail.com>
2023-06-06 22:04:49 +0530

    adding about page

--- components/basicpage/About.js
+++ components/basicpage/About.js
@@ -4,24 +4,24 @@ export default function About() {
   return (
     <section id="about" className={styles.about}>
 
-<h1 className={`${styles.heading} heading`}>about us</h1>
+      <h1 className={`${styles.heading} heading`}>about us</h1>
 
-<div className={styles.row}>
+      <div className={styles.row}>
 
-  <div className={styles.content}>
-    <h3>We build Logo Design that build your busines</h3>
-    <p>LOGO HI LOGO is spreading faster than fire  now past 3443 business logo designed! WE provide Professional, Premium  & LUXURY 2D/3D logo</p>
-    <p></p>
-    <a href="#"><button className={styles.btn}>read more</button></a>
-  </div>
+        <div className={styles.content}>
+          <h3>We build Logo Design that build your busines</h3>
+          <p>LOGO HI LOGO is spreading faster than fire  now past 3443 business logo designed! WE provide Professional, Premium  & LUXURY 2D/3D logo</p>
+          <p>Our team of dedicated designers combines their creativity and expertise to craft visually captivating logos that leave a lasting impression. We understand the power of a well-designed logo in establishing a brands identity and setting it apart from the competition.</p>
+          <a href="/abouts"><button className={styles.btn}>read more</button></a>
+        </div>
 
-  <div className="image">
-    <img src="aboutus.gif" alt="dddddddddd" width={510}/>
-  </div>
+        <div className="image">
+          <img src="aboutus.gif" alt="dddddddddd" width={510} />
+        </div>
 
-</div>
+      </div>
 
 
-</section>
+    </section>
   )
 }
