ecf9510d952a4de80905f671d5c6194885d63b07
Kavita Jakhar <Kavya21@gmail.com>
2024-07-26 14:45:31 +0530

    resolved some issues related to blog

--- pages/allblogs.js
+++ pages/allblogs.js
@@ -32,11 +32,11 @@ export default function Test() {
   };
   return (
     <>
-      <div className=" dark:text-gray-900 pt-36 flex items-center justify-center">
-        <div className="">
+      <div className=" dark:text-gray-900 pt-36 px-4 sm:px-12 flex items-center justify-center">
+        <div>
           <div className="flex flex-wrap justify-center -mx-4">
             <div className="w-full px-4">
-              <div className="text-center mx-auto mb-[7px] lg:mb-20 max-w-[510px]">
+              <div className="text-center mx-auto mb-7 lg:mb-20 max-w-[510px]">
                 <h2 className="font-bold text-4xl  sm:text-4xl text-gray-600  " style={{ 'lineHeight': '1' }}>
                   Are you a passionate reader? Read Our Latest Blogs
                 </h2>
@@ -45,11 +45,11 @@ export default function Test() {
             </div>
           </div>
           <div className="mb-4">
-            <div className="flex sm:px-2 mx-44 sm:mx-auto items-center p-6 w-fit space-x-14 bg-white rounded-xl shadow-lg hover:shadow-xl transform sm:hover:scale-x-105 transition duration-500">
+            <div className="flex flex-wrap sm:flex-nowrap mx-auto items-center p-6 w-full sm:w-fit space-y-4 sm:space-y-0 space-x-0 sm:space-x-14 bg-white rounded-xl shadow-lg hover:shadow-xl transform sm:hover:scale-105 transition duration-500">
               <div className="flex py-3 px-5 text-xl rounded-lg text-gray-500 font-medium cursor-pointer">
                 <select
                   id="underline_select"
-                  className="block py-2.5 w-full pr-5 pl-1 text-xl text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-400 focus:outline-none focus:ring-0 focus:border-gray-200 peer"
+                  className="block py-2.5 w-full pr-5 pl-1 text-lg sm:text-xl text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-400 focus:outline-none focus:ring-0 focus:border-gray-200 peer"
                   value={selectValue}
                   onChange={handleSelectChange}
                 >
@@ -62,7 +62,7 @@ export default function Test() {
                 <IoIosArrowDown className="h-7 w-7 mt-2 opacity-30" />
               </div>
               <div className="flex bg-gray-100 p-4 space-x-5 rounded-lg">
-                <FaSearch className="h-6 w-6 opacity-30" />
+                <FaSearch className="h-6 w-6 opacity-30 sm:block hidden" />
                 <input
                   className="bg-gray-100 outline-none pr-32 text-xl"
                   type={selectValue == 'date' ? 'date' : 'text'}
@@ -71,18 +71,24 @@ export default function Test() {
                   onChange={handleSearchChange}
                 />
                 {appliedFilter && <div
-                  className="bg-gray-400 py-3 px-3 text-white text-xl font-medium rounded-full hover:shadow-lg transition duration-3000 cursor-pointer"
+                  className="bg-gray-400 py-3 px-3 text-white text-xl font-medium rounded-full hover:shadow-lg transition duration-300 cursor-pointer"
                   onClick={onClearFilterClick}
                 >
                   <span><RxCross1 /></span>
                 </div>}
+                {!appliedFilter && <div
+                  className="bg-blue-400 py-3 px-3 text-white text-xl sm:hidden block font-medium rounded-full hover:shadow-lg transition duration-300 cursor-pointer"
+                  disabled={searchValue == "" && selectValue == ""} onClick={onSearchClick}
+                >
+                  <FaSearch className="h-6 w-6 text-white" />
+                </div>}
               </div>
-              <button className="bg-blue-400 py-3 px-5 text-white text-xl font-medium rounded-lg hover:shadow-lg transition duration-3000 cursor-pointer" disabled={searchValue == ""} onClick={onSearchClick}>
-                <span>Search</span>
+              <button className="bg-blue-400 py-3 px-5 sm:block hidden text-white text-xl font-medium rounded-lg hover:shadow-lg transition duration-300 cursor-pointer" disabled={searchValue == "" && selectValue == ""} onClick={onSearchClick}>
+                Search
               </button>
             </div>
           </div>
-          {posts.length === 0 && <img
+          {posts.length === 0 && !fetchListing.isLoading && <img
             src='/empty.gif'
             className='h-auto'
           />}
@@ -102,10 +108,10 @@ export default function Test() {
               </p>
             }
           >
-            <section className="pt-3 lg:pt-[12px] pb-10  lg:pb-20">
+            <section className="pt-3 lg:pt-12 pb-10  lg:pb-20">
               <div className="container m-auto">
 
-                <div className="flex flex-wrap mx-6 justify-center sm:mx-0">
+                <div className="flex flex-wrap mx-4 justify-center sm:mx-6">
                   {posts.length !== 0 &&
                     posts.map((list) => {
 
@@ -118,32 +124,25 @@ export default function Test() {
                       const maxtitle = 7;
                       const slicetit = list.title.split(' ').slice(0, maxtitle).join(' ');
                       const title = list.title.split(' ').length > slicetit ? slicetit + '...' : slicetit;
-
-
                       return (
-                        <Link href={`/blog/${list.slug}`} className="w-full shadow-lg hover:scale-105  px-5 my-10 md:w-1/2 lg:w-1/4 mx-4  " key={list._id} style={{ 'borderRadius': '10px', 'width': '380px' }}>
-                          <div className="max-w-[400px] mx-auto mb-10  ">
-                            <div className="rounded overflow-hidden mb-8 max-h-60 items-center">
-                              <img src={list.image} />
+                        <Link href={`/blog/${list.slug}`} className="w-full sm:w-1/2 lg:w-1/3 px-4 my-4 sm:my-10 mx-auto lg:min-w-[380px]" key={list._id}>
+                          <div className="shadow-lg hover:scale-105 transition duration-500" style={{ borderRadius: '10px' }}>
+                            <div className="overflow-hidden mb-8 max-h-60 items-center">
+                              <img src={list.image} className="w-full h-full object-cover" style={{ borderRadius: '10px' }} />
                             </div>
-                            <div>
-                              <span className="bg-primary rounded inline-block text-center py-1 px-4 text-2xl leading-loose font-semibold text-gray-700 mb-5">
+                            <div className="px-5 py-3">
+                              <span className="bg-primary rounded inline-block text-center py-1 px-4 text-lg sm:text-2xl leading-loose font-semibold text-gray-700 mb-5">
                                 {list.authorName}
                               </span>
-                              <span className="bg-primary rounded inline-block text-center py-1 px-4 text-2xl leading-loose font-semibold text-gray-700 mb-5">
+                              <span className="bg-primary rounded inline-block text-center py-1 px-4 text-lg sm:text-2xl leading-loose font-semibold text-gray-700 mb-5">
                                 {formattedDate}
                               </span>
-                              <h3>
-                                <div className="font-semibold text-2xl sm:text-2xl lg:text-3xl xl:text-3xl mb-4 inline-block text-dark hover:text-primary">
-                                  {title}..
-                                </div>
+                              <h3 className="font-semibold text-lg sm:text-2xl lg:text-3xl mb-4 inline-block text-dark hover:text-primary">
+                                {title}
                               </h3>
-                              <p className=" text-2xl text-body-color" >
-                                {description}...
-                                {/* <Link href={`/blog/${list.slug}`} className='bg-gray-500 p-2  text-white rounded-3xl font-medium my-3'>Read More</Link> */}
+                              <p className="text-lg sm:text-2xl text-body-color">
+                                {description}
                               </p>
-
-
                             </div>
                           </div>
                         </Link>
